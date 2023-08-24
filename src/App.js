5e426da5df62a5f8a982ca910de8f1e49a515e7c
ahmedmohamed2210 <halawaahmed507@gmail.com>
2023-08-24 08:09:40 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
+import { useState } from "react";
+
 export default function App() {
+  const [price, setPrice] = useState(" ");
+  const [percentage1, setPercentage1] = useState("");
+  const [percentage2, setPercentage2] = useState("");
+  const tip = price * ((percentage1 + percentage2) / 2 / 100);
+
+  function handleReset() {
+    setPrice("");
+    setPercentage1("0");
+    setPercentage2("0");
+  }
   return (
     <div className="App">
-      <Bill />
-      <Result />
-      <FriendService />
-      <Result />
-      <Button />
+      <Bill price={price} onChange={setPrice} />
+      <Service persantage={percentage1} onSelect={setPercentage1}>
+        How did you like the Service ?
+      </Service>
+      <Service persantage={percentage2} onSelect={setPercentage2}>
+        How did friend like the service ?
+      </Service>
+
+      {price > 0 && (
+        <>
+          <Result price={price} tip={tip} />
+          <Button onReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
 
-function Bill() {
-  return;
+function Bill({ price, onChange }) {
+  return (
+    <div>
+      <p>How much was the bill</p>
+      <input
+        type="text"
+        placeholder="Bill value"
+        value={price}
+        onChange={(e) => onChange(Number(e.target.value))}
+      ></input>
+    </div>
+  );
 }
 
-function Service() {
-  return;
+function Service({ children, persantage, onSelect }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select
+        value={persantage}
+        onChange={(e) => onSelect(Number(e.target.value))}
+      >
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5"> It was okay (5%)</option>
+        <option value="10">it was good (10%)</option>
+        <option value="15">Absolutely amazing!(20%)</option>
+      </select>
+    </div>
+  );
 }
 
-function FriendService() {
-  return;
+function Result({ price, tip }) {
+  return (
+    <div>
+      <h3>
+        you pay {price + tip} (${price} +$B {tip})
+      </h3>
+      ;
+    </div>
+  );
 }
-function Result() {}
-function Button() {
-  return;
+function Button({ onReset }) {
+  return <button onClick={onReset}>reset</button>;
 }
